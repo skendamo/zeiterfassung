2e8d479b49f68846938a318f0764de832616d6c6
skendamo <skender_morina@hotmail.com>
2026-07-01 10:34:10 +0200

    Add service worker for caching and fetch handling

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "zeiterfassung-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
+];
+
+// Dateien beim ersten Start speichern
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Dateien aus dem Cache laden
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Alten Cache löschen
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
